32260aac50c2129286fb3eca640c8b2f8b8662d4
Henil-Patel <henil.patel@hotmail.com>
2022-06-22 19:10:31 -0400

    added more functionality to class; added extensions

--- wk2/functions-and-data/src/main/scala/Main.scala
+++ wk2/functions-and-data/src/main/scala/Main.scala
@@ -2,7 +2,7 @@
   val a = Rational(1,2)
   println(a.numer)             // 1
   println(a.denom)             // 2
-  makeString(addRational(Rational(1/2), Rational(5/6)))
+  //makeString(addRational(Rational(1/2), Rational(5/6)))
   // More use cases
   val m = Rational(1,2)
   val n = Rational(2,3)
@@ -11,8 +11,19 @@
   m.add(n).multiply(o)
 
 class Rational(x: Int, y: Int):
-  def numer = x
-  def denom = y
+
+  // auxiliary constructor
+  // In this case if we just want to pass one argument i.e 2 = 2/1 technically
+  def this(x: Int) = this(x, 1)
+
+  require(y > 0, "denominator must be positive")
+
+  private def gcd(a: Int, b: Int): Int = {
+    if b == 0 then a else gcd(b, a % b)
+  }
+  //private val g = gcd(x, y)
+  val numer = x / gcd(x.abs, y)
+  val denom = y / gcd(x.abs, y)
 
   // These are methods
   def add(r: Rational) = {
@@ -20,8 +31,16 @@ class Rational(x: Int, y: Int):
              denom * r.denom)
   }
 
+  def less(that: Rational): Boolean = {
+    this.numer * that.denom < that.numer * this.denom
+  }
+
+  def max(that: Rational): Rational = {
+    if this.less(that) then that else this
+  }
+
   def multiply(r: Rational) = {
-    ???
+    (this.numer * r.numer) /  (this.denom * r.denom)
   }
 
   override def toString = s"$numer/$denom"
@@ -45,7 +64,25 @@ class Rational(x: Int, y: Int):
 
 end Rational
 
-// These are just functions:
+
+// This extends the methods of the main class
+// Note that the original class methods cannot be overriden
+extension (r: Rational)
+  // Note the infix modifier 
+  infix def min(s: Rational): Boolean = if s.less(r) then s else r
+  infix def abs: Rational = Rational(r.numer.abs, r.denom)
+  infix def + (s: Rational): Rational = r.add(s)
+  infix def * (s: Rational): Rational = r.multiply(s)
+
+
+/** Calling infix would be like:
+      r min s     instead of        r.min(s)
+      r + s       instead of        r.+(s)
+      r < s       instead of        r.<(s)
+**/
+
+
+// These are just ordinary functions:
 
 
 // def addRational(r: Rational, s: Rational): Rational = {
